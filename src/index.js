230b45458dc1ec98a78be4bf154ecada9279ddab
lutece <lallaworld1st@gmail.com>
2018-05-04 19:07:00 +0900

    complete tab list

--- src/index.js
+++ src/index.js
@@ -1,124 +1,107 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import {
-    FlatList,
-    View,
-    Text,
-    StyleSheet,
-    Dimensions,
+  FlatList,
+  View,
+  Text,
+  Dimensions,
 } from 'react-native';
 
 const { width, height } = Dimensions.get('window');
 
 export default class extends Component {
-    
-    constructor(props) {
-      super(props);
+  static defaultProps = {
+    initialNumToRender: 1,
+    maxToRenderPerBatch: 1,
+    horizontal: false,
+    scrollEnabled: false,
+    renderItem: this._deafultRenderItem,
+    data: [],
+  }
 
-      this._initiatingCheckTabSwiper();
+  static propTypes = {
+    initialNumToRender: PropTypes.number,
+    maxToRenderPerBatch: PropTypes.number,
+    horizontal: PropTypes.bool,
+    scrollEnabled: PropTypes.bool,
+    renderItem: PropTypes.func.isRequired,
+    data: PropTypes.arrayOf(PropTypes.any).isRequired,
+  }
+
+  constructor(props) {
+    super(props);
+
+    this._initiatingCheckTabSwiper();
+  }
+
+  componentWillUpdate(nextProps) {
+    const { scrollMoveToIndex } = nextProps;
+    this._scrollToIndex(scrollMoveToIndex);
+  }
+
+  _scrollToIndex = (scrollMoveToIndex) => {
+    const {
+      scrollAnimated,
+      scrollAction,
+    } = this.props;
+
+    if (scrollMoveToIndex < 0) { return; }
+    if (typeof scrollAction === 'function') scrollAction(scrollMoveToIndex);
+
+    this.tabSwiper.scrollToIndex({ animated: scrollAnimated, index: scrollMoveToIndex });
+  }
+
+  _initiatingCheckTabSwiper = () => {
+    const { data } = this.props;
+    if (!Array.isArray(data) || data.length === 0) {
+      console.error("tabSlider isn't have any data");
     }
+  }
 
-    componentDidMount() {
-      this._onLoadTabSwiper();
+  _keyExtractor = (item, index) => index;
 
-      if(this.props.debugMode) {
-        console.log('props when is called ComponentDidMount', this.props);
-        console.log('state when is called ComponentDidMount', this.state);
-      } 
-    }
+  _getItemLayout = (data, index) => {
+    const {
+      horizontal,
+    } = this.props;
 
-    componentWillUpdate(nextProps, nextState) {
-        if(nextProps.scrollToIndex) {
-            this._scrollToIndex(nextProps.scrollToIndex);
-        }
-    }
-    
-    _scrollToIndex = ({animated, index}) => {
-        console.log(index)
-        this.tabSwiper.scrollToIndex({animated, index});
-    }
+    const ITEM_LENGHT = horizontal ? width : height;
 
-    _onLoadTabSwiper = () => {
-        const { onLoadTabSwiper } = this.props;
+    return {
+      length: ITEM_LENGHT,
+      offset: ITEM_LENGHT * index,
+      index,
+    };
+  }
 
-        if(onLoadTabSwiper === undefined) {
-            return;
-        }
+  _deafultRenderItem = ({ item }) => (
+    <View style={{ width }}>
+      {typeof item === 'object' ? item : <Text>{item}</Text>}
+    </View>
+  )
 
-        if(typeof onLoadTabSwiper !== 'function') {
-            console.error("onLoadTabSwiper is a function which called when 'tabSwiepr' is mounted.")
-            return;
-        }
+  render() {
+    const {
+      data,
+      horizontal,
+      scrollEnabled,
+      renderItem,
+      initialNumToRender,
+      maxToRenderPerBatch,
+    } = this.props;
 
-        onLoadTabSwiper();
-    }
-
-    _initiatingCheckTabSwiper = () => {
-        const { data, renderItem } = this.props;
-        if(!Array.isArray(data) || data.length === 0) {
-            console.error("tabSlider isn't have any data")
-        }
-    }
-
-    _keyExtractor = (item, index) => {
-        return index + Math.random() + '';
-    }
-
-    _getItemLayout = (data, index) => {
-        
-        const ITEM_LENGHT = this.props.horizontal ? width : height;
-
-        return {
-            length: ITEM_LENGHT,
-            offset: ITEM_LENGHT * index, 
-            index 
-        }
-    }
-
-    _deafultRenderItem = ({item, index}) => (
-        <View style={styles.slide}>
-            {typeof item === 'object' ? item : <Text>{item}</Text>}
-        </View>
-    )
-
-    render() {
-
-       const { 
-           data, 
-           renderItem,
-           horizontal,
-           scrollEnabled,
-        } = this.props;
-        
-       return (
-         <FlatList
-            styles={styles.container}
-            ref={list => { this.tabSwiper = list}}
-            data={data}
-            initialNumToRender={1}
-            renderItem={renderItem ? renderItem : this._deafultRenderItem}
-            getItemLayout={this._getItemLayout}
-            horizontal={horizontal || false}
-            keyExtractor={this._keyExtractor}
-            scrollEnabled={scrollEnabled || false}
-         />
-       )
-    }
+    return (
+      <FlatList
+        data={data}
+        scrollEnabled={scrollEnabled}
+        horizontal={horizontal}
+        renderItem={renderItem}
+        getItemLayout={this._getItemLayout}
+        keyExtractor={this._keyExtractor}
+        initialNumToRender={initialNumToRender}
+        maxToRenderPerBatch={maxToRenderPerBatch}
+        ref={(listNode) => { this.tabSwiper = listNode; }}
+      />
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        width,
-        height,
-        backgroundColor: 'transparent',
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    slide: {
-        width,
-        height,
-        backgroundColor: 'transparent',
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-});
